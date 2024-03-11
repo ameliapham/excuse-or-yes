2346d7e6fbfbb3a619d83fe8a05c3757689709e9
ameliapham <ptt.huong.8895@gmail.com>
2024-03-11 01:50:30 +0100

    Add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider
+    theme={createTheme({
+      palette: {
+        primary: {
+          main: "#003049"
+        },
+        secondary: {
+          main: "#bde0fe"
+        },
+        background: {
+          default: "#ffe5ec",
+          paper: "#ffc8dd",
+        }
+      },
+      typography: {
+        fontFamily: "geist",
+        fontSize: 16,
+      },      
+    })}
+  >
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 )
