a76ab09a95edf9ccec428a65c40a64339a19a1de
ameliapham <ptt.huong.8895@gmail.com>
2024-03-11 01:50:48 +0100

    Add title heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react"
+import { useTheme } from "@mui/material/styles"
+import { useStyles } from "tss-react"
+
 export function App() {
-  return <h1>Hello World</h1>
+
+  const theme = useTheme();
+
+  const { css } = useStyles();
+
+  return (
+    <div
+      className={css({
+        "backgroundColor": theme.palette.background.default,
+        "color": theme.palette.text.primary,
+        "fontFamily": theme.typography.fontFamily,
+        "fontSize": theme.typography.fontSize,
+        "height": "100vh",
+        "display": "flex",
+        "alignItems": "center",
+        "justifyContent": "center",
+      })}
+    >
+
+      <h1
+        className={css({
+          "fontSize": theme.spacing(4),
+          "font": theme.typography.fontFamily,
+          "color": theme.palette.primary.main,
+        })}>
+        Vas-tu m'excuser ?
+      </h1>
+
+   
+
+    </div>
+  );
+
 }
