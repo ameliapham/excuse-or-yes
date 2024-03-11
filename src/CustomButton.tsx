a9a5dbce4d3ff9de0441cc8d1ff8275f5385db8b
ameliapham <ptt.huong.8895@gmail.com>
2024-03-11 20:06:23 +0100

    Update CustomButton : Add function ButtonZone

--- src/CustomButton.tsx
+++ src/CustomButton.tsx
@@ -3,16 +3,59 @@ import Button from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
 import { useStyles } from "tss-react";
 
+type Props = {
+    className?: string;
+    sticker: "first" | "second" | "third";
+    onPageChange: (sticker: "first" | "second" | "third") => void;
+}
+
+export function ButtonZone(props: Props) {
+    const { className, sticker, onPageChange } = props;
+
+    const { css, cx } = useStyles();
+
+    return (
+        <div
+            className={css({
+                "width": "200px",
+                "display": "flex",
+                "flexDirection": "row",
+                "justifyContent": "space-around",
+                "marginBottom": "20px",
+            })}
+        >
+            <CustomButton
+                onClick={() => onPageChange("second")}
+                onMouseOver={() => {
+                    onPageChange("second");
+                }}
+                onMouseOut={() => { 
+                    onPageChange("first");
+                }}
+            >
+                Yes
+            </CustomButton>
+            <CustomButton
+                onClick={() => onPageChange(sticker)}
+                onMouseOver={() => {}}
+            >
+                Non
+            </CustomButton>
+        </div>
+    )
+}
 
 type CustomButtonProps = {
     className?: string;
-    selected: boolean;
+    //selected: boolean;
     children: React.ReactNode;
     onClick: () => void;
+    onMouseOver: () => void;
+    onMouseOut?: () => void;
 }
 
 export function CustomButton(props: CustomButtonProps) {
-    const { className, onClick, children, selected } = props;
+    const { className, onClick, children, onMouseOver, onMouseOut } = props;
 
     const { css, cx } = useStyles();
 
@@ -22,19 +65,24 @@ export function CustomButton(props: CustomButtonProps) {
         <Button
             className={cx(css({
                 "&:hover": {
-                    backgroundColor: theme.palette.primary.main,
+                    "backgroundColor": theme.palette.primary.main,
                     "color": theme.palette.secondary.light,
                 },
                 //"backgroundColor": theme.palette.primary.main,
                 //"color": theme.palette.secondary.light,
+                "border": "1px solid",
                 "padding": theme.spacing(1),
                 "paddingLeft": theme.spacing(3),
                 "paddingRight": theme.spacing(3),
+                "transition": "ease-in-out .3s",
             }), className)}
-            variant={selected ? "contained" : "outlined"}
+            //variant={selected ? "contained" : "outlined"}
             onClick={onClick}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
         >
             {children}
         </Button >
     )
 }
+
