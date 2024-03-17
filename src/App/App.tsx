ac207506ff95f7fe99123c98827e7b2d36de7aaa
ameliapham <ptt.huong.8895@gmail.com>
2024-03-17 19:54:19 +0100

    Add function onStickerChange and customize button Oui

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,20 @@
 import { tss } from "tss-react/mui";
 import { theme } from "../theme";
 import { GlobalStyles } from "tss-react";
-import { CustomButton } from "../components/CustomButton";
-import { FirstSticker } from "../pages/FirstSticker";
 import { useState } from "react";
 
-export function App() {
-    const { cx, classes } = useStyles();
+import { CustomButton } from "../components/CustomButton";
+import { FirstSticker } from "../shared/FirstSticker";
+import { SecondSticker } from "../shared/SecondSticker";
+import { Call } from "../pages/Call";
 
+export function App() {
+    const { classes } = useStyles();
+
+    const [sticker, setStickers] = useState<"first" | "second" | "Call">("first");
+
+    const onStickerChange = (sticker: "first" | "second" | "Call") => {setStickers(sticker)}
+    
     return (
         <div className={classes.root}>
             <GlobalStyles
@@ -22,12 +29,25 @@ export function App() {
                 }}
             />
 
-            <FirstSticker></FirstSticker>
+            <div>
+                {(() => {
+                    switch (sticker) {
+                        case "first":
+                            return <FirstSticker />;
+                        case "second":
+                            return <SecondSticker />;
+                        case "Call":
+                            return <Call />;
+                    }
+                })()}
+            </div>
 
             <div className = {classes.buttonZone}>
                 <CustomButton
                     variant="contained"
-                    onClick={() => console.log("Hello World")}
+                    onMouseOver={() => onStickerChange("second")}
+                    onMouseOut={() => onStickerChange("first")}
+                    onClick={() => onStickerChange("Call")}
                 >
                     Oui
                 </CustomButton>
