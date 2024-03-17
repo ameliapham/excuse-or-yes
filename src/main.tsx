b1ca5fe4b86589b6458470730cc9318816909331
ameliapham <ptt.huong.8895@gmail.com>
2024-03-17 17:31:11 +0100

    Create theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { App } from './App/App'
+import { theme } from './theme'
+import { ThemeProvider } from "@mui/material/styles"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider
-    theme={createTheme({
-      palette: {
-        primary: {
-          main: "#003049"
-        },
-        secondary: {
-          main: "#bde0fe"
-        },
-        background: {
-          default: "#ffe5ec",
-          paper: "#ffc8dd",
-        }
-      },
-      typography: {
-        fontFamily: "geist",
-        fontSize: 16,
-      },      
-    })}
-  >
+  <ThemeProvider theme={theme}>
     <App />
+    
   </ThemeProvider>,
 )
---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles"
+
+export const theme = createTheme({
+    "palette": {
+        "primary": {
+            "main": "#003049"
+        },
+        "secondary": {
+            "main": "#bde0fe"
+        },
+        "background": {
+            "default": "#ffe5ec",
+            "paper": "#ffc8dd",
+        }
+    },
+    "typography": {
+        "fontFamily": "geist",
+        "fontSize": 16,
+    },      
+})
