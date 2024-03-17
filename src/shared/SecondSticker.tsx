a6afb1e8864a54c0539649564e708f8b41309a86
ameliapham <ptt.huong.8895@gmail.com>
2024-03-17 19:59:26 +0100

    Customize secondSticker

---
+++ src/shared/SecondSticker.tsx
@@ -0,0 +1,38 @@
+import  Typography  from "@mui/material/Typography"
+import { theme } from "../theme"
+import { tss } from "tss-react/mui"
+
+
+export function SecondSticker() {
+    const { classes } = useStyles();
+
+    return (
+        <div
+            className={classes.root}
+        >
+            <Typography
+                variant="h4"
+                color="primary"
+                className={classes.typography}
+            >
+                Really ?
+            </Typography>
+
+            <div>
+                <img src="https://media.giphy.com/media/eHWUyxudOQGCulzFFR/giphy.gif" alt="memes cries" />
+            </div>
+        </div>
+    )
+}
+
+const useStyles = tss.create(({theme}) => ({
+    "root": {
+        "display": "flex",
+        "flexDirection": "column",
+        "alignItems": "center",
+    },
+    "typography": {
+        "fontFamily": theme.typography.fontFamily,
+        "marginTop": theme.spacing(10),
+    }
+}));
