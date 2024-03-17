b5f0c713daf6415acaa9e03f0237c56e2eb00bb1
ameliapham <ptt.huong.8895@gmail.com>
2024-03-17 21:39:46 +0100

    Add page Call

---
+++ src/pages/Call.tsx
@@ -0,0 +1,92 @@
+import { theme } from "../theme"
+import { tss } from "tss-react/mui"
+import { GlobalStyles } from "@mui/material"
+import { CustomButton } from "../components/CustomButton"
+import { Success } from "./Success"
+import { ThirdSticker } from "../shared/ThirdSticker"
+import { useState } from "react";
+
+
+export function Call() {
+    const { css, classes } = useStyles();
+
+    const [page, setPage] = useState<"Call" | "Success">("Call");
+
+    const onPageChange = (page: "Call" | "Success") => { setPage(page) };
+
+    const [buttonStyle, setButtonStyle] = useState({});
+
+    const moveButton = () => {
+        const x = Math.random() * (window.innerWidth - 100);
+        const y = Math.random() * (window.innerHeight - 50);
+        setButtonStyle({
+            "position": "absolute",
+            "left": `${x}px`,
+            "top": `${y}px`,
+            "transition": 'left 0.5s, top 0.5s ease-in-out'
+        });
+    }
+
+    if (page === "Success") {
+        return <Success />
+    }
+    return (
+        <div className={classes.root}>
+
+            <GlobalStyles
+                styles={{
+                    "html, body": {
+                        "margin": 0,
+                        "display": "flex",
+                        "justifyContent": "center",
+                        "backgroundColor": theme.palette.background.default,
+                        "alignItems": "center"
+                    }
+                }}
+            />
+
+            <ThirdSticker />
+
+
+            <div className={classes.buttonZone}>
+                <CustomButton
+                    variant="contained"
+                    onClick={() => onPageChange("Success")}
+                >
+                    Oui
+                </CustomButton>
+
+                <CustomButton
+                    variant="contained"
+                    onMouseOver={moveButton}
+                    onClick={moveButton}
+                    className={css(buttonStyle)}
+                >
+                    Non
+                </CustomButton>
+            </div>
+
+        </div>
+
+
+    )
+}
+
+const useStyles = tss.create(({ theme }) => ({
+    "root": {
+        "display": "flex",
+        "flexDirection": "column",
+        "alignItems": "center",
+    },
+    "typography": {
+        "fontFamily": theme.typography.fontFamily,
+        "marginTop": theme.spacing(10),
+    },
+    "buttonZone": {
+        "display": "block flex",
+        "justifyContent": "space-between",
+        "width": "150px",
+        "alignItems": "center",
+        "marginTop": "10px"
+    }
+}));
