3c86eabcf6b632b51073fedbb791b3a123e72091
ameliapham <ptt.huong.8895@gmail.com>
2024-03-19 03:14:56 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App/App'
 import { theme } from './theme'
 import { ThemeProvider } from "@mui/material/styles"
+import { I18nFetchingSuspense } from './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <App />
-    
-  </ThemeProvider>,
+  <I18nFetchingSuspense fallback={null}>
+    <ThemeProvider theme={theme}>
+      <App />
+
+    </ThemeProvider>
+  </I18nFetchingSuspense>
 )
