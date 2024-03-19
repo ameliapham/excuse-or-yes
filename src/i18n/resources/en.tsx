d35fad6993364d65803de3a92c0a5b1939ac64a3
ameliapham <ptt.huong.8895@gmail.com>
2024-03-19 03:14:39 +0100

    Add resources of translate

---
+++ src/i18n/resources/en.tsx
@@ -0,0 +1,21 @@
+import type { Translations } from '../types';
+
+export const translations: Translations<"en"> = {
+    "App": {
+        "Yes": "Yes",
+        "No": "No",
+    },
+    "FirstSticker": {
+        "Will you excuse me?": "Will you excuse me?"
+    },
+    "SecondSticker": {
+        "Really?": "Really?"
+    },
+    "ThirdSticker": {
+        "Will you call me?": "Will you call me?"
+    },
+    "Call": {
+        "Yes": "Yes",
+        "No": "No"
+    }
+};
---
+++ src/i18n/resources/fr.tsx
@@ -0,0 +1,23 @@
+import type { Translations } from '../types';
+
+export const translations: Translations<"fr"> = {
+    /* spell-checker: disable */
+    "App": {
+        "Yes": "Oui",
+        "No": "Non",
+    },
+    "FirstSticker": {
+        "Will you excuse me?": "Vas-tu m'excuser ?"
+    },
+    "SecondSticker": {
+        "Really?": "Vraiment ?"
+    },
+    "ThirdSticker": {
+        "Will you call me?": "Tu m'appelleras ?"
+    },
+    "Call": {
+        "Yes": "Oui",
+        "No": "Non"
+    }
+    /* spell-checker: enable */
+};
